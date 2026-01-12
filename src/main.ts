5bcbaa35751c351118bb1ed30da024b57fdc79da
BLeo16 <blfloresa@utn.edu.ec>
2026-01-12 15:08:19 -0500

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,11 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true,
   }))
-  await app.listen(3000);
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  });
+
+  await app.listen(3001);
 }
 bootstrap();
